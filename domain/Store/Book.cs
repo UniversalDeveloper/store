735fc4c306b4196663d2771675d30fb3500eb3b4
lopata_anastasiia <lopata.ad@gmail.com>
2020-11-27 13:38:03 +0200

    Описать сущность Book

---
+++ domain/Store/Book.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace store
+{
+    public class Book
+
+    {public int Id { get; }
+        public string Title { get; }
+        public Book(int id,string title) {
+            Title = title;
+            Id = id;
+        }
+    }
+}
