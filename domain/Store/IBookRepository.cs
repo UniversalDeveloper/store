cb4facf8447c60d74b3983063b4bf272342ff305
lopata_anastasiia <lopata.ad@gmail.com>
2020-11-27 18:37:56 +0200

    Добавить интерфейс репозитория книг

---
+++ domain/Store/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace store
+{
+  public interface IBookRepository
+    {
+        Book[] GetAllByTitle(string titlePart); 
+    }
+}
