464762bc05b2451c19018da3b9297496f3adbf15
lopata_anastasiia <lopata.ad@gmail.com>
2020-12-01 14:48:40 +0200

    Добавить реализацию репозитория книги в память

---
+++ infrastructure/Store_Mamory/BookRepository.cs
@@ -0,0 +1,20 @@
+﻿using store;
+using System;
+using System.Linq;
+
+namespace Store_Mamory
+{
+    public class BookRepository : IBookRepository
+    {
+        private readonly Book[] books = new[] { 
+        new Book(1, "Art of Programing"),
+         new Book(2, "Refactorng"),
+          new Book(3, "C Programing Language"),
+        };
+
+        public Book[] GetAllByTitle(string titlePart)
+        {
+            return books.Where(book => book.Title.Contains(titlePart)).ToArray();
+        }
+    }
+}
