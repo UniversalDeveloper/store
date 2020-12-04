9b4aebf8157d0fc510f192b23383145037de9e33
lopata_anastasiia <lopata.ad@gmail.com>
2020-12-04 12:31:57 +0200

    Добавить страницу поиска с подключением учебного хранилища в контролере

--- presentation/Store.Web/Startup.cs
+++ presentation/Store.Web/Startup.cs
@@ -8,6 +8,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using store;
+using Store_Mamory;
 
 namespace Store.Web
 {
@@ -24,6 +26,7 @@ namespace Store.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddSingleton<IBookRepository, BookRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
